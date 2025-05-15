cedb0c51348567a1abcf6f54214ccd8de0bdaaa0
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-15 21:47:52 +0300

    practices using file IO

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode = "w", encoding = "utf-8") as file_name:
+        file_name.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode = "a", encoding= "utf-8") as file_name:
+        file_name.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode= "r", encoding= "utf-8") as file_name:
+        return file_name.read()
+
+write_file("testfile", "This is line 1\n")
+append_file("testfile", "This is line 2\n")
+
+content = read_file("testfile")
+
+print(content)
